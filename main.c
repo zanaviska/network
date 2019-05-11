51590fafce07030195577cf9f15e027c89fd469f
Andrii <dlink.uaua@gmail.com>
2019-05-11 12:11:59 +0000

    added avl tree

--- main.c
+++ main.c
@@ -21,9 +21,9 @@ void print_icmp_packet(unsigned char* , int);
 void PrintData (unsigned char* , int);
 
 int sock_raw;
-FILE *logfile;
 int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;
 struct sockaddr_in source,dest;
+char iface[10];
 
 int start();
 int stop();
@@ -32,9 +32,182 @@ int select_iface();
 int stat();
 int help();
 
+// to store all ip count for iface and have a quick access I use avl tree
+// the key of sort is our ip
+// start of tree realization
+struct node 
+{ 
+    int ip;
+	int count;
+    struct node *left; 
+    struct node *right; 
+    int height; 
+}; 
+  
+// A utility function to get the height of the tree 
+int height(struct node *n) 
+{ 
+    if (n == NULL) 
+        return 0; 
+    return n->height; 
+} 
+  
+// A utility function to get maximum of two integers 
+int max(int a, int b) 
+{ 
+    return (a > b)? a : b; 
+} 
+  
+/* Helper function that allocates a new node with the given key and 
+    NULL left and right pointers. */
+struct node* newnode(int ip, int count) 
+{ 
+    struct node* node = (struct node*) 
+                        malloc(sizeof(struct node)); 
+    node->ip     = ip; 
+	node->count  = count;
+    node->left   = NULL; 
+    node->right  = NULL; 
+    node->height = 1;  // new node is initially added at leaf 
+    return(node); 
+} 
+  
+// A utility function to right rotate subtree rooted with y 
+// See the diagram given above. 
+struct node *rightRotate(struct node *y) 
+{ 
+    struct node *x = y->left; 
+    struct node *t2 = x->right; 
+  
+    // Perform rotation 
+    x->right = y; 
+    y->left = t2; 
+  
+    // Update heights 
+    y->height = max(height(y->left), height(y->right))+1; 
+    x->height = max(height(x->left), height(x->right))+1; 
+  
+    // Return new root 
+    return x; 
+} 
+  
+// A utility function to left rotate subtree rooted with x 
+// See the diagram given above. 
+struct node *leftRotate(struct node *x) 
+{ 
+    struct node *y = x->right; 
+    struct node *t2 = y->left; 
+  
+    // Perform rotation 
+    y->left = x; 
+    x->right = t2; 
+  
+    //  Update heights 
+    x->height = max(height(x->left), height(x->right))+1; 
+    y->height = max(height(y->left), height(y->right))+1; 
+  
+    // Return new root 
+    return y; 
+} 
+  
+// Get Balance factor of node N 
+int getBalance(struct node *n) 
+{ 
+    if (n == NULL) 
+        return 0; 
+    return height(n->left) - height(n->right); 
+} 
+  
+// Recursive function to insert a key in the subtree rooted 
+// with node and returns the new root of the subtree. 
+struct node* insert(struct node* node, int ip, int count) 
+{ 
+    /* 1.  Perform the normal BST insertion */
+    if (node == NULL) 
+        return(newnode(ip, count)); 
+  
+    if (ip < node->ip) 
+        node->left  = insert(node->left, ip, count); 
+    else if (ip > node->ip) 
+        node->right = insert(node->right, ip, count); 
+    else // Equal keys are not allowed in BST 
+        return node; 
+  
+    /* 2. Update height of this ancestor node */
+    node->height = 1 + max(height(node->left), height(node->right)); 
+  
+    /* 3. Get the balance factor of this ancestor 
+          node to check whether this node became 
+          unbalanced */
+    int balance = getBalance(node); 
+  
+    // If this node becomes unbalanced, then 
+    // there are 4 cases 
+  
+    // Left Left Case 
+    if (balance > 1 && ip < node->left->ip) 
+        return rightRotate(node); 
+  
+    // Right Right Case 
+    if (balance < -1 && ip > node->right->ip) 
+        return leftRotate(node); 
+  
+    // Left Right Case 
+    if (balance > 1 && ip > node->left->ip) 
+    { 
+        node->left = leftRotate(node->left); 
+        return rightRotate(node); 
+    } 
+  
+    // Right Left Case 
+    if (balance < -1 && ip < node->right->ip) 
+    { 
+        node->right = rightRotate(node->right); 
+        return leftRotate(node); 
+    } 
+  
+    /* return the (unchanged) node pointer */
+    return node; 
+} 
+  
+// A utility function to print preorder traversal 
+// of the tree. 
+// The function also prints height of every node 
+void preOrder(struct node *root) 
+{ 
+    if(root != NULL) 
+    { 
+        printf("%d(%d) ", root->ip, root->count); 
+        preOrder(root->left); 
+        preOrder(root->right); 
+    } 
+} 
+
+//function that increase ip's counter
+void increase(struct node *root, int ip)
+{
+	if(root->ip == ip)
+	{
+		root->count++;
+		return;
+	}
+	if(ip < root->ip)
+	{
+		if(root->left == NULL)
+			root->left = newnode(ip, 0);
+		increase(root->left, ip);
+	} else
+	{
+		if(root->right == NULL)
+			root->right = newnode(ip, 0);
+		increase(root->right, ip);
+	}
+}
+
+//end of tree realization
+
 int main(int argc, char* argv[])
 {
-	//solve();
 	if(argc < 2)
 	{
 		printf("Error: you should write parametr\n");
@@ -56,9 +229,7 @@ int start()
 	struct in_addr in;
 	int count = 0;
 	unsigned char *buffer = (unsigned char *)malloc(65536); //Its Big!
-	
-	//logfile=fopen("log.txt","w");
-	//if(logfile==NULL) printf("Unable to create file.");
+
 	printf("Starting...\n");
 	//Create a raw socket that shall sniff
 	sock_raw = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
@@ -74,7 +245,7 @@ int start()
 	FILE* system_input = fopen("system.txt", "r");
 	char iface[10];
 	fscanf(system_input, "%s", &iface);
-	printf("%s\n", iface);
+	fclose(system_input);
 	snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), iface);
 	if (setsockopt(sock_raw, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
 		printf("Problem with conection to interface\n");
@@ -93,7 +264,10 @@ int start()
 		}
 		//Now process the packet
 		//ProcessPacket(buffer , data_size);
-		printf("There is %d packets \n\r", ++count);
+		struct iphdr *iph = (struct iphdr *)buffer;
+		memset(&source, 0, sizeof(source));
+		source.sin_addr.s_addr = iph->saddr;
+		printf("There is %d packets  and last is from %s \n\r", ++count, inet_ntoa(source.sin_addr));
 
 	}
 	close(sock_raw);
@@ -103,7 +277,7 @@ int start()
 
 int stop()
 {
-
+	return 0;
 }
 
 int show()
@@ -113,163 +287,16 @@ int show()
 
 int select_iface(char* iface)
 {
-	stop();
+	int not_stoped = stop();
 	FILE* system_input;
 	system_input = fopen("system.txt", "w");
 	fprintf(system_input, "%s\n", iface);
 	fclose(system_input);
-	start();
+	if(!not_stoped)
+		start();
 }
 
 int help()
 {
 
-}
-
-void ProcessPacket(unsigned char* buffer, int size)
-{
-	//Get the IP Header part of this packet
-	struct iphdr *iph = (struct iphdr*)buffer;
-	++total;
-	switch (iph->protocol) //Check the Protocol and do accordingly...
-	{
-		case 1:  //ICMP Protocol
-			++icmp;
-			//PrintIcmpPacket(buffer,size);
-			break;
-		
-		case 2:  //IGMP Protocol
-			++igmp;
-			break;
-		
-		case 6:  //TCP Protocol
-			++tcp;
-			print_tcp_packet(buffer , size);
-			break;
-		
-		case 17: //UDP Protocol
-			++udp;
-			//print_udp_packet(buffer , size);
-			break;
-		
-		default: //Some Other Protocol like ARP etc.
-			++others;
-			break;
-	}
-	print_ip_header(buffer, size);
-	printf("TCP : %d   UDP : %d   ICMP : %d   IGMP : %d   Others : %d   Total : %d \n\r",tcp,udp,icmp,igmp,others,total);
-}
-
-void print_ip_header(unsigned char* Buffer, int Size)
-{
-	unsigned short iphdrlen;
-		
-	struct iphdr *iph = (struct iphdr *)Buffer;
-	iphdrlen =iph->ihl*4;
-	
-	memset(&source, 0, sizeof(source));
-	source.sin_addr.s_addr = iph->saddr;
-	
-	memset(&dest, 0, sizeof(dest));
-	dest.sin_addr.s_addr = iph->daddr;
-	
-	fprintf(logfile,"\n");
-	fprintf(logfile,"IP Header\n");
-	fprintf(logfile,"   |-IP Version        : %d\n",(unsigned int)iph->version);
-	fprintf(logfile,"   |-IP Header Length  : %d DWORDS or %d Bytes\n",(unsigned int)iph->ihl,((unsigned int)(iph->ihl))*4);
-	fprintf(logfile,"   |-Type Of Service   : %d\n",(unsigned int)iph->tos);
-	fprintf(logfile,"   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(iph->tot_len));
-	fprintf(logfile,"   |-Identification    : %d\n",ntohs(iph->id));
-	//fprintf(logfile,"   |-Reserved ZERO Field   : %d\n",(unsigned int)iphdr->ip_reserved_zero);
-	//fprintf(logfile,"   |-Dont Fragment Field   : %d\n",(unsigned int)iphdr->ip_dont_fragment);
-	//fprintf(logfile,"   |-More Fragment Field   : %d\n",(unsigned int)iphdr->ip_more_fragment);
-	fprintf(logfile,"   |-TTL      : %d\n",(unsigned int)iph->ttl);
-	fprintf(logfile,"   |-Protocol : %d\n",(unsigned int)iph->protocol);
-	fprintf(logfile,"   |-Checksum : %d\n",ntohs(iph->check));
-	fprintf(logfile,"   |-Source IP        : %s\n",inet_ntoa(source.sin_addr));
-	fprintf(logfile,"   |-Destination IP   : %s\n",inet_ntoa(dest.sin_addr));
-}
-
-void print_tcp_packet(unsigned char* Buffer, int Size)
-{
-	unsigned short iphdrlen;
-	
-	struct iphdr *iph = (struct iphdr *)Buffer;
-	iphdrlen = iph->ihl*4;
-	
-	struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen);
-			
-	fprintf(logfile,"\n\n***********************TCP Packet*************************\n");	
-		
-	print_ip_header(Buffer,Size);
-		
-	fprintf(logfile,"\n");
-	fprintf(logfile,"TCP Header\n");
-	fprintf(logfile,"   |-Source Port      : %u\n",ntohs(tcph->source));
-	fprintf(logfile,"   |-Destination Port : %u\n",ntohs(tcph->dest));
-	fprintf(logfile,"   |-Sequence Number    : %u\n",ntohl(tcph->seq));
-	fprintf(logfile,"   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
-	fprintf(logfile,"   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
-	//fprintf(logfile,"   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
-	//fprintf(logfile,"   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
-	fprintf(logfile,"   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
-	fprintf(logfile,"   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
-	fprintf(logfile,"   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
-	fprintf(logfile,"   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
-	fprintf(logfile,"   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
-	fprintf(logfile,"   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
-	fprintf(logfile,"   |-Window         : %d\n",ntohs(tcph->window));
-	fprintf(logfile,"   |-Checksum       : %d\n",ntohs(tcph->check));
-	fprintf(logfile,"   |-Urgent Pointer : %d\n",tcph->urg_ptr);
-	fprintf(logfile,"\n");
-	fprintf(logfile,"                        DATA Dump                         ");
-	fprintf(logfile,"\n");
-		
-	fprintf(logfile,"IP Header\n");
-	PrintData(Buffer,iphdrlen);
-		
-	fprintf(logfile,"TCP Header\n");
-	PrintData(Buffer+iphdrlen,tcph->doff*4);
-		
-	fprintf(logfile,"Data Payload\n");	
-	PrintData(Buffer + iphdrlen + tcph->doff*4 , (Size - tcph->doff*4-iph->ihl*4) );
-						
-	fprintf(logfile,"\n###########################################################");
-}
-
-void PrintData (unsigned char* data , int Size)
-{
-	
-	for(i=0 ; i < Size ; i++)
-	{
-		if( i!=0 && i%16==0)   //if one line of hex printing is complete...
-		{
-			fprintf(logfile,"         ");
-			for(j=i-16 ; j<i ; j++)
-			{
-				if(data[j]>=32 && data[j]<=128)
-					fprintf(logfile,"%c",(unsigned char)data[j]); //if its a number or alphabet
-				
-				else fprintf(logfile,"."); //otherwise print a dot
-			}
-			fprintf(logfile,"\n");
-		} 
-		
-		if(i%16==0) fprintf(logfile,"   ");
-			fprintf(logfile," %02X",(unsigned int)data[i]);
-				
-		if( i==Size-1)  //print the last spaces
-		{
-			for(j=0;j<15-i%16;j++) fprintf(logfile,"   "); //extra spaces
-			
-			fprintf(logfile,"         ");
-			
-			for(j=i-i%16 ; j<=i ; j++)
-			{
-				if(data[j]>=32 && data[j]<=128) fprintf(logfile,"%c",(unsigned char)data[j]);
-				else fprintf(logfile,".");
-			}
-			fprintf(logfile,"\n");
-		}
-	}
 }
